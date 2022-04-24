2b5d7db14e44e0e40b608956b4389cc7e5ffcc02
Ralph Nahra <rallph@protonmail.com>
2022-04-23 21:52:05 -0500

    add parse log file

--- log_entries.py
+++ log_entries.py
@@ -57,3 +57,13 @@ def parse_log_line(log_line):
     if LogEntry.LOG_ENTRY_REGEX.match(log_line):
         return LogEntry.from_log_string(log_line)
 
+def parse_log_file(log_file):
+
+    entries = []
+    for line in log_file.readlines():
+        entry = parse_log_line(line)
+
+        if entry:
+            entries.append(entry)
+
+    return entries
--- test/test_log_entries.py
+++ test/test_log_entries.py
@@ -1,5 +1,6 @@
 import unittest
 import log_entries
+import io
 
 class TestParseLogLine(unittest.TestCase):
 
@@ -13,3 +14,49 @@ class TestParseLogLine(unittest.TestCase):
         not_log_line = "This is not a valid log entry"
         entry = log_entries.parse_log_line(not_log_line)
         self.assertEqual(entry, None)
+
+class TestParseLogFile(unittest.TestCase):
+
+
+    def test_parse_log_file_contiguous_entries(self):
+        lines = [
+            '2021-02-09 13:10:55.909 test entry 1',
+            '2021-02-09 13:10:56.012 test entry 2',
+            '2021-02-09 13:10:56.018 test entry 3'
+        ]
+
+        log_file = io.StringIO('\n'.join(lines))
+        entries = log_entries.parse_log_file(log_file)
+
+        self.assertEqual(len(entries), 3)
+        for entry in entries:
+            self.assertIsInstance(entry, log_entries.LogEntry)
+
+    def test_parse_log_file_divided_entries(self):
+        lines = [
+            '2021-02-09 13:10:55.909 test entry 1',
+            'not an entry',
+            '2021-02-09 13:10:56.012 test entry 2',
+            'not an entry',
+            'not an entry',
+            '2021-02-09 13:10:56.018 test entry 3'
+        ]
+
+        log_file = io.StringIO('\n'.join(lines))
+        entries = log_entries.parse_log_file(log_file)
+
+        self.assertEqual(len(entries), 3)
+        for entry in entries:
+            self.assertIsInstance(entry, log_entries.LogEntry)
+        
+    def test_parse_log_file_no_entries(self):
+        lines = [
+            'not an entry',
+            'not an entry',
+            'not an entry'
+        ]
+
+        log_file = io.StringIO('\n'.join(lines))
+        entries = log_entries.parse_log_file(log_file)
+
+        self.assertEqual(len(entries), 0)
